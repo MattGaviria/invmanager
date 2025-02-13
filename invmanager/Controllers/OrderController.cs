c07f76f6c4fb1a872a15caff1824f8417de84f05
Ginageegee <regina.slonimsky@hotmail.com>
2025-02-13 14:06:47 -0500

    "order commit"

---
+++ invmanager/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using invmanager.Models;
+using invmanager.Data;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace invmanager.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+        
+
+
+
+
+ 
+        
+        
+
+
+
+    }
+
+}
---
+++ invmanager/Models/Order.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace invmanager.Models
+{
+    public class Order 
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }   
+
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }  
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1.")]
+        public int Quantity { get; set; }
+
+        public double OrderTotal { get; set; } 
+
+        [Required]
+        public string Status { get; set; } = "Pending"; 
+
+        [Required]
+        
+        public string CustomerName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string CustomerEmail { get; set; }
+    }
+}
